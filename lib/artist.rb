4f77d7aa58745d074028852a8f5a38b77508910b
chris brady <chrisb3546@yahoo.com>
2020-04-11 20:54:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+
+attr_accessor :name, :songs
+
+def initialize(name)
+  @name = name 
+  #@songs = []
+end 
+
+#artist = Artist.new(name)
+def songs 
+  Song.all.select do |song|
+    song.artist == self
+end 
+end 
+
+def add_song(song)
+  song.artist = self
+end 
+
+def add_song_by_name(song_name)
+  singer = Song.new(song_name)
+  add_song(singer)
+end 
+
+def song_count
+  
+
+
+end
