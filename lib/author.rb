ceff6a282e5b57e486b82098c0b9e76c1ca22cfb
chris brady <chrisb3546@yahoo.com>
2020-04-11 21:09:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,8 +18,8 @@ def add_post(post)
   post.author = self
 end 
 
-def add_post_by_title(post_title)
-  writer = Post.new(post_title)
+def add_post_by_title(title)
+  writer = Post.new(title)
   add_post(writer)
 end 
 
